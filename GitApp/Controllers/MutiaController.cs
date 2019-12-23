0f49885e1fd2ded8ea36078e1e8d64ce5d10ab47
mutiasha <shafirach98@gmail.com>
2019-12-23 15:47:27 +0700

    initial commit

---
+++ GitApp/Controllers/MutiaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitApp.Controllers
+{
+    public class MutiaController : Controller
+    {
+        //
+        // GET: /Mutia/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
